627a4d16a418ae511f3de8bfb7ad94d2ed2bfcc7
Ivan Apet <van.ka515@gmail.com>
2017-11-15 15:55:13 +0300

    WalletCell done
    Add portfolioCell

---
+++ Multy/Cells/WalletTVC/WalletTableViewCell.swift
@@ -0,0 +1,26 @@
+//Copyright 2017 Idealnaya rabota LLC
+//Licensed under Multy.io license.
+//See LICENSE for details
+
+import UIKit
+
+class WalletTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tokenImage: UIImageView!
+    @IBOutlet weak var walletNameLbl: UILabel!
+    @IBOutlet weak var cryptoSumLbl: UILabel!
+    @IBOutlet weak var cryptoNameLbl: UILabel!
+    @IBOutlet weak var fiatSumLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    
+}
--- Multy/ViewControllers and Presenters/Assets/AssetsViewController.swift
+++ Multy/ViewControllers and Presenters/Assets/AssetsViewController.swift
@@ -4,10 +4,60 @@
 
 import UIKit
 
-class AssetsViewController: UIViewController {
-
+class AssetsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewTopConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.checkOSForConstraints()
+        self.registerCells()
+    }
+    
+    func checkOSForConstraints() {
+        if #available(iOS 11.0, *) {
+            //OK: Storyboard was made for iOS 11
+        } else {
+            self.tableViewTopConstraint.constant = 0
+        }
+    }
+    
+    func registerCells() {
+        let walletCell = UINib.init(nibName: "WalletTableViewCell", bundle: nil)
+        self.tableView.register(walletCell, forCellReuseIdentifier: "walletCell")
+        
+        let portfolioCell = UINib.init(nibName: "PortfolioTableViewCell", bundle: nil)
+        self.tableView.register(portfolioCell, forCellReuseIdentifier: "portfolioCell")
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath == [0, 0] {
+            let portfolioCell = self.tableView.dequeueReusableCell(withIdentifier: "portfolioCell") as! PortfolioTableViewCell
+            return portfolioCell
+        } else {
+            let walletCell = self.tableView.dequeueReusableCell(withIdentifier: "walletCell") as! WalletTableViewCell
+            
+            return walletCell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath == [0,0] {
+            return 283
+        } else {
+            return 104
+        }
+    }
+    
 }
