d1bbfe9ce869a5856ceb1e7aeea61ed5810f7b96
iD3V <iprodev78@gmail.com>
2017-11-17 15:08:41 +0300

    added pharse functionality

--- Multy/ViewControllers and Presenters/Seed Phrase/Phrase Word/SeedPhraseWordViewController.swift
+++ Multy/ViewControllers and Presenters/Seed Phrase/Phrase Word/SeedPhraseWordViewController.swift
@@ -12,19 +12,27 @@ class SeedPhraseWordViewController: UIViewController {
     @IBOutlet weak var mediumWordLbl: UILabel!
     @IBOutlet weak var bottomWord: UILabel!
     
-    var countOfTaps = 0
+    var countOfTaps = -1
+    let seedPhraseArray = ["soda", "siren", "victory", "ozone", "document", "attract", "dragon", "innocent", "aisle", "shallow", "elbow", "make", "organ", "version", "equip"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextWordAndContinueAction(UIButton());
     }
 
     @IBAction func nextWordAndContinueAction(_ sender: Any) {
         if self.countOfTaps == 3 {
             self.nextWordBtn.setTitle("Continue", for: .normal)
         }
+        
         //getNextWords
         if self.countOfTaps < 4 {
             self.countOfTaps += 1
+            
+            self.topWordLbl.text = seedPhraseArray[3 * countOfTaps]
+            self.mediumWordLbl.text = seedPhraseArray[3 * countOfTaps + 1]
+            self.bottomWord.text = seedPhraseArray[3 * countOfTaps + 2]
         }  else {
             self.performSegue(withIdentifier: "backupSeedPhraseVC", sender: UIButton.self)
         }
