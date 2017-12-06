d8fca682530a64970ce0d2ac864c46276fdfbb5d
iD3V <iprodev78@gmail.com>
2017-12-06 16:41:58 +0300

    updated socket class

--- Multy/DataMangers/Socket.swift
+++ Multy/DataMangers/Socket.swift
@@ -8,26 +8,28 @@ import SocketIO
 class Socket: NSObject {
     static let shared = Socket()
     
+    
+    let manager = SocketManager(socketURL: URL(string: "http://192.168.0.121:6666/socket.io/")!, config: [.log(true), .compress, .forceWebsockets(true), .reconnectAttempts(3), .forcePolling(false)])
+    
     func start() {
-        let manager = SocketManager(socketURL: URL(string: "http://192.168.0.109:6666/socket.io/")!, config: [.log(true), .compress, .forceWebsockets(true), .reconnectAttempts(3), .forcePolling(false)])
+
         let socket = manager.defaultSocket
         
         socket.on(clientEvent: .connect) {data, ack in
             print("socket connected")
         }
         
-        socket.on("currentAmount") {data, ack in
-            guard let cur = data[0] as? Double else { return }
-            
-            socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
-                socket.emit("update", ["amount": cur + 2.50])
-            }
-            
-            ack.with("Got your currentAmount", "dude")
-        }
+//        socket.on("currentAmount") {data, ack in
+//            guard let cur = data[0] as? Double else { return }
+//
+//            socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+//                socket.emit("update", ["amount": cur + 2.50])
+//            }
+//
+//            ack.with("Got your currentAmount", "dude")
+//        }
         
         socket.connect()
+        
     }
-    
-    
 }
