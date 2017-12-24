a6aca4b2168fd8767d8a4cd7913d7a8f834697cd
Ivan Apet <van.ka515@gmail.com>
2017-12-24 13:23:12 +0300

    Remove recent addresses

--- Multy/ViewControllers and Presenters/Send/SendStart/SendStartViewController.swift
+++ Multy/ViewControllers and Presenters/Send/SendStart/SendStartViewController.swift
@@ -89,7 +89,7 @@ extension SendStartViewController:  UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
