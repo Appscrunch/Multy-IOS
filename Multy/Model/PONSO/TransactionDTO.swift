1a65f9f41ea2f2f8ec6e874cc17085fe3fb8b8db
Alex Pro <alex78pro@gmail.com>
2018-02-13 19:00:50 +0300

    updated transactionDTO

--- Multy/Model/PONSO/TransactionDTO.swift
+++ Multy/Model/PONSO/TransactionDTO.swift
@@ -7,11 +7,22 @@ import Foundation
 class TransactionDTO: NSObject {
     var currencyID : NSNumber? {
         didSet {
-            
+            createTransactionDTO()
         }
     }
     
     var transactionDTO: BaseTransactionDTO?
+    
+    func createTransactionDTO() {
+        switch currencyID?.uint32Value {
+        case CURRENCY_BITCOIN.rawValue?:
+            transactionDTO = BTCTransactionDTO()
+        case CURRENCY_ETHEREUM.rawValue?:
+            transactionDTO = ETHTransactionDTO()
+        default:
+            transactionDTO = BTCTransactionDTO()
+        }
+    }
 }
 
 class BaseTransactionDTO {
