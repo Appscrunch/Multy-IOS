7765a28ccab3b073ea491411a8645e39c0ab7216
Alex Pro <alex78pro@gmail.com>
2018-03-01 19:13:21 +0300

    Added test files

---
+++ MultyTests/MultyTests.swift
@@ -0,0 +1,37 @@
+//Copyright 2018 Idealnaya rabota LLC
+//Licensed under Multy.io license.
+//See LICENSE for details
+
+import XCTest
+@testable import Multy
+
+class MultyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample(string: inout String) {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let core = DataMan
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+            
+        }
+    }
+    
+}
